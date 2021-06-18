6838e28a5cf75ad3bcb9d00b8ea9a037d2ecd526
叶江 <1140549742@qq.com>
2021-06-19 02:51:03 +0800

    重构项目结构

--- springboot-security/src/main/java/com/ApplicationSecutiry.java
+++ springboot-security/src/main/java/com/ApplicationSecutiry.java
@@ -1,4 +1,4 @@
-package com.muyer;
+package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,10 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version
  */
 
-@SpringBootApplication(scanBasePackages = {"com.muyer"})
-public class Application {
+@SpringBootApplication(scanBasePackages = {"com.muyer.*"})
+public class ApplicationSecutiry {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(ApplicationSecutiry.class, args);
     }
 }
--- springboot-security/src/main/java/com/muyer/controller/LoginController.java
+++ springboot-security/src/main/java/com/muyer/controller/LoginController.java
@@ -2,9 +2,11 @@ package com.muyer.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Description: 
@@ -17,18 +19,36 @@ public class LoginController {
     private Logger logger = LoggerFactory.getLogger(LoginController.class);
 
 
-    @RequestMapping("/")
-    public String home() {
-        String name = SecurityContextHolder.getContext().getAuthentication().getName();
-        logger.info("当前登陆用户：" + name);
-        return "home.html";
+//    @RequestMapping("/")
+//    public String home() {
+//        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+//        logger.info("当前登陆用户：" + name);
+//        return "home.html";
+//    }
+
+    @RequestMapping({"/", "/index"})
+    public String index(Model model) {
+        model.addAttribute("msg", "welcome you!");
+        return "login";
     }
 
 
-    @RequestMapping("/login")
+    @GetMapping("/test/login")
     public String login(){
-        return "login.html";
+        return "login";
     }
 
+    @GetMapping("/admin")
+    @ResponseBody
+//    @PreAuthorize("hasRole('ROLE_admin')")
+    public String printAdmin() {
+        return "ROLE_admin角色";
+    }
 
+    @GetMapping("/user")
+    @ResponseBody
+//    @PreAuthorize("hasRole('ROLE_user')")
+    public String printUser() {
+        return "ROLE_user角色";
+    }
 }
